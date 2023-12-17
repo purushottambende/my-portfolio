c92ca5654a2aeb00c340cdfaa26f3dc10ca05341
Harshal Aglawe <147182253+harshaal06@users.noreply.github.com>
2023-12-17 15:34:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-new js file create
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabContents = document.getElementsByClassName("tab-contents");
+function opentab(tabName){
+            for(tabLink of tabLinks){
+                tabLink.classList.remove("active-link");
+            }
+            for(tabContent of tabContents){
+                tabContent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabName).classList.add("active-tab");
+        }
+        let sidemeu = document.getElementById("sideMenu");
+        function openMenu(){
+            sidemeu.style.right = "0";
+        }
+        function closeMenu(){
+            sidemeu.style.right = "-200px";
+        }
