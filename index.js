cb21774e75747ab4dd0d2e14e17ab91d26ce1de8
Harshal Aglawe <147182253+harshaal06@users.noreply.github.com>
2023-12-17 15:36:56 +0530

    JavaScript changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 let tabLinks = document.getElementsByClassName("tab-links");
 let tabContents = document.getElementsByClassName("tab-contents");
 function opentab(tabName){
-            for(tabLink of tabLinks){
+         for(tabLink of tabLinks){
                 tabLink.classList.remove("active-link");
             }
-            for(tabContent of tabContents){
+         for(tabContent of tabContents){
                 tabContent.classList.remove("active-tab");
             }
             event.currentTarget.classList.add("active-link");
             document.getElementById(tabName).classList.add("active-tab");
-        }
-        let sidemeu = document.getElementById("sideMenu");
-        function openMenu(){
-            sidemeu.style.right = "0";
-        }
-        function closeMenu(){
-            sidemeu.style.right = "-200px";
-        }
+}
+let sidemeu = document.getElementById("sideMenu");
+function openMenu(){
+        sidemeu.style.right = "0";
+}
+function closeMenu(){
+        sidemeu.style.right = "-200px";
+}
