064eac2f56eb48c2d0ae063a8090f7fc4400fe5f
harshaal06 <harshalaglawe06@gmail.com>
2024-01-09 15:53:32 +0530

    contact change

--- index.js
+++ index.js
@@ -17,19 +17,3 @@ function openMenu() {
 function closeMenu() {
         sidemeu.style.right = "-200px";
 }
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwBTipdlnKJSzTsYT0rYPPKPeqavEQ810aazmbAusRhfzOwH2AAoaDsJ_Xc5ikS8wckKg/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-
-form.addEventListener('submit', e => {
-        e.preventDefault()
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-                .then(response => {
-                        msg.innerHTML = "Message send successfully"
-                        setTimeout(function(){
-                                msg.innerHTML = ""
-                        },5000)
-                        form.reset()
-                })
-                .catch(error => console.error('Error!', error.message))
-})
